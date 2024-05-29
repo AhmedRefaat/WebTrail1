86eb362c50f3333a226b3311edc7c45658e25f81
ahmedrmohamed <ahmd.refat@gmail.com>
2024-05-29 23:54:07 +0200

    add the first job table and migrate it

--- job/models.py
+++ job/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+JOB_TYPE = (
+    ('FULL TIME', 'FULL TIME'),
+    ('PART TIME', 'PART TIME'),
+)
+
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=15, choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    Vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+
+
+    def __str__(self) -> str:
+        return self.title
