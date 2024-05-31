b7f48f583fbf47524705b33fa21e61994bb8441b
ahmedrmohamed <ahmd.refat@gmail.com>
2024-05-31 20:12:28 +0200

    Preapre the Job detail rendering

--- job/views.py
+++ job/views.py
@@ -10,4 +10,6 @@ def job_list(request):
     return render(request, 'job/job_list.html', context)
 
 def job_detail(request, id):
-    pass
+    job_detail = Job.objects.get(id=id)
+    context = {'job':job_detail}
+    return render(request, 'job/job_detail.html', context)
